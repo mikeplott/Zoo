2a5d7f53176c4c92e44afd430914bf0d3c87e1e3
michaelplott <mike.plott80@gmail.com>
2016-09-20 12:06:05 -0400

    Added factory method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,5 +16,29 @@ public class Main {
         System.out.println(d);
         System.out.println(h);
         System.out.println(s);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type an animal name:");
+        String name = scanner.nextLine();
+        Animal a = createAnimal(name);
+        System.out.println(a);
+    }
+
+    // factory method
+    static Animal createAnimal(String name) {
+        Animal animal;
+        if (name.equalsIgnoreCase("dog")) {
+            animal = new Dog();
+        }
+        else if (name.equalsIgnoreCase("hawk")) {
+            animal = new Hawk();
+        }
+        else if (name.equalsIgnoreCase("sname")) {
+            animal = new Snake();
+        }
+        else {
+            animal = new Animal();
+        }
+        return animal;
     }
 }
